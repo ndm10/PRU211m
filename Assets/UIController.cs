ca9d5109990c8d4a3d3ad9f96c586150284d1774
Hung Thanh <hungthanh15022001@gmail.com>
2023-07-12 01:42:14 +0700

    update menu

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -25,7 +25,7 @@ public class UIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
